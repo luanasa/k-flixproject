908501cd97cf7162daaa5fb0237b7c2a86d9a26c
Luana Martins <luamartinstravel@gmail.com>
2023-06-17 17:46:25 -0300

    mobile version

--- assets/js/owl/setup.js
+++ assets/js/owl/setup.js
@@ -30,3 +30,8 @@ function logarUser() {
     window.location.replace("/index.html");
 }
 
+$(function() {                       //run when the DOM is ready
+    $(".mobile-menu").click(function() {  //use a class, since your ID gets mangled
+      $(this).addClass("links-mob");      //add the class to the clicked element
+    });
+  });
